e2c1c527df42ea04077eb4db526b0a1c1f847419
fabriken <techtux@gmail.com>
2021-09-19 17:05:56 +0930

    Update index.py
    Added test mode option.

--- index.py
+++ index.py
@@ -11,11 +11,13 @@ user = Client(api_key, api_secret) # Client for user endpoints.
 public = Client()                  # Client for public endpoints.
 fee = 0.00075                      # 0.075% in decimal. Set trading fee here, differs between different VIP levels.
 
+test_mode = 0                      # Place a test order instead of a real one. 0 = off | 1 = 0 on
+
 PAIRS = {                          # Define pairs to trade here:
     'GALAUSDT': {                  # PAIRS.keys()
         'symbol': 'GALA',          # Make sure symbol and base are set to the same as the PAIRS.keys().
         'base': 'USDT',            # Make sure symbol and base are set to the same as the PAIRS.keys().
-        'kline': '@kline_1m',      # Set kline interval here.
+        'kline': '@kline_15m',     # Set kline interval here.
         'price': '@bookTicker',    # Set price ticker here.
         'decPrice': 5,             # Count how many decimals the price has and set here.
         'decOrder': 0,             # Same as decPrice, but for the order.
@@ -34,7 +36,7 @@ PAIRS = {                          # Define pairs to trade here:
         'closes': []
     },
     'CELRUSDT':    {
-        'kline': '@kline_1m',
+        'kline': '@kline_1h',
         'price': '@bookTicker',
         'decPrice': 5,
         'decOrder': 1,
@@ -50,13 +52,12 @@ PAIRS = {                          # Define pairs to trade here:
         'symbol': 'CELR',
         'base': 'USDT',
         'awaitOrder': False,
-        'hodl': 0,
         'lastPrice': 0,
         'profit': 1 + fee, # Set profit margin here.
         'closes': []
     },
     'DYDXUSDT':    {
-        'kline': '@kline_1m',
+        'kline': '@kline_1h',
         'price': '@bookTicker',
         'decPrice': 3,
         'decOrder': 2,
@@ -72,13 +73,12 @@ PAIRS = {                          # Define pairs to trade here:
         'symbol': 'DYDX',
         'base': 'USDT',
         'awaitOrder': False,
-        'hodl': 0,
         'lastPrice': 0,
         'profit': 1 + fee, # Set profit margin here.
         'closes': []
     },
     'SHIBUSDT':    {
-        'kline': '@kline_1m',
+        'kline': '@kline_15m',
         'price': '@bookTicker',
         'decPrice': 8,
         'decOrder': 0,
@@ -94,9 +94,8 @@ PAIRS = {                          # Define pairs to trade here:
         'symbol': 'SHIB',
         'base': 'USDT',
         'awaitOrder': False,
-        'hodl': 0,
         'lastPrice': 0,
-        'profit': 1 + fee, # Set profit margin here.
+        'profit': 2 + fee, # Set profit margin here.
         'closes': []
     }
 }
@@ -213,17 +212,28 @@ def order(side, symbol, price, qty):
         dec = PAIRS[i]['decPrice']
     # Binance API won't accept scientific notation.
     # If price has been formatted to scientific notation, format back to decimal. Ex. 8.04e-06 to 0.00000804.
+    # * Python formats numbers with more than 5 decimals to scientific notation.
     if float(price) < float(0.00001):
         price = f"{price:.{dec}f}"
     try:
-        # order = user.create_test_order( # Test order
-        order = user.create_order( # Real order
-            symbol=symbol,
-            side=side,
-            type=ORDER_TYPE_LIMIT,
-            timeInForce=TIME_IN_FORCE_GTC,
-            quantity=qty,
-            price=price)               
+        if test_mode == 1:
+            print('TEST MODE ACTIVE, NO ORDERS WILL BE EXECUTED! Disable by setting test_mode = 0')
+            order = user.create_test_order( # Test order
+                symbol=symbol,
+                side=side,
+                type=ORDER_TYPE_LIMIT,
+                timeInForce=TIME_IN_FORCE_GTC,
+                quantity=qty,
+                price=price)
+            print(order)
+        else:
+            user.create_order( # Real order
+                symbol=symbol,
+                side=side,
+                type=ORDER_TYPE_LIMIT,
+                timeInForce=TIME_IN_FORCE_GTC,
+                quantity=qty,
+                price=price)
     except Exception as e:
         print(e)
         return False
@@ -294,72 +304,72 @@ def ws():
                     if is_candle_closed:
                         PAIRS[i]['closes'].append(truncate(float(close) , PAIRS[i]['decPrice']))
 
-                        if len(PAIRS[i]['closes']) >= PAIRS[i]['smaHigh']:
-                                np_closes = numpy.array(PAIRS[i]['closes'])
-                                sma_low = talib.SMA(np_closes, timeperiod=PAIRS[i]['smaLow'])
-                                sma_high = talib.SMA(np_closes, timeperiod=PAIRS[i]['smaHigh'])
-                                last_sma_low = truncate(sma_low[-1], PAIRS[i]['decPrice'])
-                                last_sma_high = truncate(sma_high[-1],  PAIRS[i]['decPrice'])
-                                # last_sma_low = 60 # Test values
-                                # last_sma_high = 55 # Test values
+                if len(PAIRS[i]['closes']) >= PAIRS[i]['smaHigh']:
+                        np_closes = numpy.array(PAIRS[i]['closes'])
+                        sma_low = talib.SMA(np_closes, timeperiod=PAIRS[i]['smaLow'])
+                        sma_high = talib.SMA(np_closes, timeperiod=PAIRS[i]['smaHigh'])
+                        last_sma_low = truncate(sma_low[-1], PAIRS[i]['decPrice'])
+                        last_sma_high = truncate(sma_high[-1],  PAIRS[i]['decPrice'])
+                        # last_sma_low = 60 # Test values
+                        # last_sma_high = 55 # Test values
+                        
+                        if PAIRS[i]['bid'] and PAIRS[i]['ask']: # Make sure we have prices.
+
+
+                            if PAIRS[i]['order'] == 0: # If not in order, create an inital order, buy or sell, but await signal first:
+                                assetBal = float(PAIRS[i]['assetBal'])
+                                bid = PAIRS[i]['bid']
+                                ask = PAIRS[i]['ask']
+                                    # div = div - 1
                                 
-                                if PAIRS[i]['bid'] and PAIRS[i]['ask']: # Make sure we have prices.
+                            if assetBal * ask > minNotional: # Place SELL ORDER if true.
+                                # div = div - 1
+                                if PAIRS[i]['decOrder'] == 0:
+                                    qty = int(assetBal)
+                                else:
+                                    qty = truncate(assetBal, PAIRS[i]['decOrder'])
+                                    
+                                if PAIRS[i]['awaitOrder'] == False:
+                                    if last_sma_low < last_sma_high: # SELL SIGNAL - if true
+                                        # if PAIRS[i]['lastPrice'] == 0:
+                                            pair = symbol + base
+                                            load_buy = load_csv('BUY', pair)
+                                            if load_buy: # If load_buy is successful, base the order from past trade:
 
-
-                                    if PAIRS[i]['order'] == 0: # If not in order, create an inital order, buy or sell, but await signal first:
-                                        assetBal = float(PAIRS[i]['assetBal'])
-                                        bid = PAIRS[i]['bid']
-                                        ask = PAIRS[i]['ask']
-                                            # div = div - 1
-                                        
-                                    if assetBal * ask > minNotional: # Place SELL ORDER if true.
-                                        # div = div - 1
-                                        if PAIRS[i]['decOrder'] == 0:
-                                            qty = int(assetBal)
-                                        else:
-                                            qty = truncate(assetBal, PAIRS[i]['decOrder'])
-                                            
-                                        if PAIRS[i]['awaitOrder'] == False:
-                                            if last_sma_low < last_sma_high: # SELL SIGNAL - if true
-                                                # if PAIRS[i]['lastPrice'] == 0:
-                                                    pair = symbol + base
-                                                    load_buy = load_csv('BUY', pair)
-                                                    if load_buy: # If load_buy is successful, base the order from past trade:
-
-                                                        if (ask - load_buy) / ask * 100 > PAIRS[i]['profit']:
-                                                            PAIRS[i]['awaitOrder'] = True
-                                                            gain = '{:.2f}'.format((ask - load_buy) / ask * 100)
-
-                                                            print(f'\nSelling {assetBal} {symbol} at: {ask}, bought at: {load_buy}')
-                                                            print(f'Potential gain: {gain}%')
-
-                                                            order(SIDE_SELL, PAIRS[i]['symbol'] + PAIRS[i]['base'], ask, qty)
-
-                                                    else: # Just place an order, this will be the starting point for the trading:
-                                                            PAIRS[i]['awaitOrder'] = True
-                                                            print(f'\nSelling: {assetBal} {symbol} at {ask}, no past buy trade logged.')
-                                                            order(SIDE_SELL, PAIRS[i]['symbol'] + PAIRS[i]['base'], ask, qty)
-
-                                    else: # place BUY ORDER
-                                        if div > 1:
-                                            baseBal = float(PAIRS[i]['baseBal']) / div
-                                            bid = PAIRS[i]['bid']
-                                        else:
-                                            baseBal = float(PAIRS[i]['baseBal'])
-                                            bid = PAIRS[i]['bid']
-
-                                        if PAIRS[i]['decOrder'] == 0:
-                                            qty = int(baseBal / bid)
-                                        else:
-                                            qty = truncate(baseBal / bid, PAIRS[i]['decOrder'])
-
-                                        if PAIRS[i]['awaitOrder'] == False:
-                                            if last_sma_low > last_sma_high: # BUY SIGNAL - if true
-                                                if baseBal > minNotional:
+                                                if (ask - load_buy) / ask * 100 > PAIRS[i]['profit']:
                                                     PAIRS[i]['awaitOrder'] = True
-                                                    print('Dividend:', div)
-                                                    print(f'\nBuying: {qty} {symbol} with {baseBal} {base} at {bid}')
-                                                    order(SIDE_BUY, PAIRS[i]['symbol'] + PAIRS[i]['base'], bid, qty)
+                                                    gain = '{:.2f}'.format((ask - load_buy) / ask * 100)
+
+                                                    print(f'\nSelling {assetBal} {symbol} at: {ask}, bought at: {load_buy}')
+                                                    print(f'Potential gain: {gain}%')
+
+                                                    order(SIDE_SELL, PAIRS[i]['symbol'] + PAIRS[i]['base'], ask, qty)
+
+                                            else: # Just place an order, this will be the starting point for the trading:
+                                                    PAIRS[i]['awaitOrder'] = True
+                                                    print(f'\nSelling: {assetBal} {symbol} at {ask}, no past buy trade logged.')
+                                                    order(SIDE_SELL, PAIRS[i]['symbol'] + PAIRS[i]['base'], ask, qty)
+
+                            else: # place BUY ORDER
+                                if div > 1:
+                                    baseBal = float(PAIRS[i]['baseBal']) / div
+                                    bid = PAIRS[i]['bid']
+                                else:
+                                    baseBal = float(PAIRS[i]['baseBal'])
+                                    bid = PAIRS[i]['bid']
+
+                                if PAIRS[i]['decOrder'] == 0:
+                                    qty = int(baseBal / bid)
+                                else:
+                                    qty = truncate(baseBal / bid, PAIRS[i]['decOrder'])
+
+                                if PAIRS[i]['awaitOrder'] == False:
+                                    if last_sma_low > last_sma_high: # BUY SIGNAL - if true
+                                        if baseBal > minNotional:
+                                            PAIRS[i]['awaitOrder'] = True
+                                            print('Dividend:', div)
+                                            print(f'\nBuying: {qty} {symbol} with {baseBal} {base} at {bid}')
+                                            order(SIDE_BUY, PAIRS[i]['symbol'] + PAIRS[i]['base'], bid, qty)
 
         twm.start_user_socket(callback=userData)
         join_streams = []
