1fd3d9921674e5a1cc4c3e19389a54f45338c67a
fabriken <techtux@gmail.com>
2021-09-20 19:01:52 +0930

    Update install-as-systemd-service.sh
    Change to have a separate directory in systemd for sma-trader

--- install-as-systemd-service.sh
+++ install-as-systemd-service.sh
@@ -1,10 +1,13 @@
 #!/bin/bash +x
 
+mkdir /etc/systemd/system/sma-trader
+
 cp sma-trader.service /etc/systemd/system/
-cp sma-trader-app.py /etc/systemd/system/
+cp sma-trader-app.py /etc/systemd/system/sma-trader/
+cp config.py /etc/systemd/system/sma-trader/
 
 systemctl enable sma-trader.service
 systemctl start sma-trader.service
 
 echo "Installed SMA trader systemd service successfully."
-echo "Run sudo systemctl status sma-trader to check status."
+systemctl status sma-trader.service
